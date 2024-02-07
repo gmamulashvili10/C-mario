efdc9dce636d9d72096b67058790df79ced78f3b
gmamulashvili10 <giomamula20040211@gmail.com>
2024-02-07 14:41:59 +0400

    Add C-mario

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //height of input
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height <= 0 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        //height pyramid print
+        for (int j = 0; j < height; j++)
+        {
+            if (i + j < height - 1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
